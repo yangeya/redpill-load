efeb7f79596633ccfab9d74c130d66655e904b06
dogodefi <dogodefi@gmail.com>
2022-05-16 14:43:05 +0800

    import @jumkey's jun method in 42218 version.

--- buildroot/board/syno/rootfs-overlay/root/init.sh
+++ buildroot/board/syno/rootfs-overlay/root/init.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+cat /proc/cmdline | grep -q "whosyourdaddy"
+if [ $? -eq 0 ]; then
+  echo "Debug mode, you will stay at the first kernel"
+  exit 0
+fi
+
 cd $(dirname $0)
 mkdir /temp1
 mkdir /temp2
 # TODO sdb?
-usbfs=$(fdisk -l /dev/sd* | grep '128 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
+usbfs=$(fdisk -l /dev/sd* | grep -E '128 MB|160 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
 wait_time=30
 time_counter=0
 while [ "${usbfs}" = "" ] && [ $time_counter -lt $wait_time ]; do
   sleep 1
-  usbfs=$(fdisk -l /dev/sd* | grep '128 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
+  usbfs=$(fdisk -l /dev/sd* | grep -E '128 MB|160 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
   echo "Still waiting for boot device (waited $((time_counter=time_counter+1)) of ${wait_time} seconds)"
 done
 mount ${usbfs}1 /temp1
@@ -28,7 +34,7 @@ unlzma < /temp2/rd.gz | cpio -idmuv
 
 unlzma < /temp1/custom.gz | cpio -idmuv
 
-# make any modifications and recompress \/ 
+# rd.gz + custom.gz
 find . 2>/dev/null | cpio -o -H newc -R root:root | xz -9 --format=lzma > ../rd.gz
 
 cd ..
@@ -37,8 +43,19 @@ cd ..
 dd if=/dev/zero of=rd.gz bs=68 count=1 conv=notrunc oflag=append
 
 rm -rf ramdisk
+
+# rebuild zImage
+if [[ -f "/temp1/tools/vmlinux-to-bzImage.sh" ]]; then
+  echo -n "Rebuilding zImage using external template..."
+  /temp1/tools/vmlinux-to-bzImage.sh
+else
+  echo -n "Rebuilding zImage... "
+  ./vmlinux-to-bzImage.sh
+fi
+
 umount /temp1
 umount /temp2
 
-kexec -d --args-linux ./vmlinux.bin --type=elf-x86_64 --reuse-cmdline --initrd=./rd.gz
+# start
+kexec -d --args-linux ./zImage --type=bzImage64 --reuse-cmdline --initrd=./rd.gz
 kexec -d -e
